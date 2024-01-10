ee12aa6362f195f4a37bcfb326e6cd8bb12ebb35
piyushraj2340 <piyushraj2340@gmail.com>
2024-01-10 17:45:51 +0530

    update the db error

--- src/db/db.js
+++ src/db/db.js
@@ -3,12 +3,9 @@ const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
 
 // console.log(process.env.COLLECTION_NAME);
-const DB = `mongodb+srv://${process.env.COLLECTION_NAME}:${process.env.COLLECTION_PASSWORD}@assignment.irsabln.mongodb.net/?retryWrites=true&w=majority`;
+const DB = `mongodb+srv://${process.env.COLLECTION_NAME}:${process.env.COLLECTION_PASSWORD}@cluster0.vmmpqyi.mongodb.net/y`;
 
-mongoose.connect(DB, {
-    useNewUrlParser: true,
-
-}).then(() => {
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("connection successful!...");
 }).catch((err) => {
     console.log(`connection failed!.... ${err}`);
