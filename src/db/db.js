c9277ab8a59b56ebc2c081cb9c3ccd56edbb0e39
piyushraj2340 <piyushraj2340@gmail.com>
2024-01-10 18:04:05 +0530

    updated the error in database

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,7 @@ mongoose.set("strictQuery", false);
 // console.log(process.env.COLLECTION_NAME);
 const DB = `mongodb+srv://${process.env.COLLECTION_NAME}:${process.env.COLLECTION_PASSWORD}@cluster0.vmmpqyi.mongodb.net/y`;
 
-mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect(DB).then(() => {
     console.log("connection successful!...");
 }).catch((err) => {
     console.log(`connection failed!.... ${err}`);
